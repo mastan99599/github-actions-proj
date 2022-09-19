111b6226cd39c101f8d5db4bd7ba45ca7e25ef79
shaikmatan504@gmail.com <shaikmastan504@gmail.com>
2022-09-19 09:11:36 +0530

    created CICD resource

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source             = "../ec2"
+  source             = "./ec2"
   for_each           = var.shaik_loop
   ami_name           = each.value.ami
   instance_type      = each.value.instance_type
